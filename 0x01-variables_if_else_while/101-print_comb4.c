988b7de714aee312fe4ba07e67582fca12c187a2
EgedovoEveweroJoy <egedovoevewerojoy57@gmail.com>
2022-12-11 14:52:49 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,33 +11,33 @@
  */
 int main(void)
 {
-	int x = '0';
-	int y, z;
+	int num1 = '0';
+	int num2, num3;
 
-	while (x <= 9)
+	while (num1 <= 9)
 	{
-		y = 0;
-		while (y <= 9)
+		num2 = 0;
+		while (num2 <= 9)
 		{
-			z = 0;
-			while (z <= 9)
+			num3 = 0;
+			while (num3 <= 9)
 			{
-				if (x != y && x < y && y != z && y < z)
+				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
 				{
-					putchar(x + 48);
-					putchar(y + 48);
-					putchar(z + 48);
-					if (x + y + z != 24)
+					putchar(num1 + 48);
+					putchar(num2 + 48);
+					putchar(num3 + 48);
+					if (num1 + num2 + num3 != 24)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-				++z;
+				++num3;
 			}
-			++y;
+			++num2;
 		}
-		++x;
+		++num1;
 	}
 	putchar('\n');
 	return (0);
